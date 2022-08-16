5b590fa4576e6632e11f09db491940c5457ac3ac
ercantekeli <ercantekeli@outlook.com>
2022-08-16 23:10:00 +0300

    15th project first commit

---
+++ 015-cocktails/src/pages/About.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit iure doloremque quaerat, doloribus laborum ratione ipsam quod voluptates inventore nulla hic! Dolorum, accusantium! Fuga repellat magnam quibusdam cum. Praesentium perferendis quos ducimus unde quidem repellat provident repellendus sit natus. Esse?
+      </p>
+    </section>
+  )
+}
+
+export default About
